9ccdb68168f54dc6c63eaa14faf74341a412b4ea
Taekyung Heo <tkheo@casys.kaist.ac.kr>
2020-08-24 20:17:22 +0900

    Add cloudsuite

---
+++ cloudsuite/build.sh
@@ -0,0 +1,2 @@
+docker build -t cloudsuite/graph-analytics:latest cloudsuite/benchmarks/graph-analytics/3.0 &
+docker build -t cloudsuite/in-memory-analytics:latest cloudsuite/benchmarks/in-memory-analytics/3.0 &
